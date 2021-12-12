eff3282c4d1d19e57fe6aaf63ced8dc887a20a47
Hamid Djennaoui <h.djennaoui@effy.fr>
2021-12-12 03:30:01 +0100

    style header

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
 <div class=" border-2 border-black row justify-between h-24">
-  <div class="row justify-between flex-1 px-8">
-    <div class="h-6">
-      <HdjnLogo/>
+  <div class="row flex-1">
+    <div class="h-6 px-8 row justify-center border-r-2 border-black h-full">
+      <div class=" h-6">  <HdjnLogo/></div>
     </div>
-    <div>
+    <div class="flex-1 bg-red-50 bg-opacity-40 h-full row justify-center">
       <a href="" v-for="index in 3" :key="index" class=" px-4 mx-2 text-xs uppercase font-black">lien</a>
     </div>
 
